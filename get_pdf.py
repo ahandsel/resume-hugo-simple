a6cb6a057968cfbccf81d0b8063a53a63cfda02b
Genji <genji-fujimori@cybozu.co.jp>
2024-07-14 14:59:18 +0900

    test

--- get_pdf.py
+++ get_pdf.py
@@ -5,12 +5,14 @@ import uuid
 
 uid = uuid.uuid4()
 from datetime import datetime
+
 current_date = datetime.now()
 at = current_date.isoformat()
 
 
 API_KEY = os.environ.get("PDFCO_KEY")
 URL = os.environ.get("RESUME_URL")
+
 
 def get(fmt="Letter"):
     config = {
@@ -24,7 +26,7 @@ def get(fmt="Letter"):
         "mediaType": "print",
         "async": False,
         "encrypt": False,
-        "profiles": "{ \"CustomScript\": \";; // put some custom js script here \"}"
+        "profiles": '{ "CustomScript": ";; // put some custom js script here "}',
     }
     url = "https://api.pdf.co/v1/pdf/convert/from/url"
     r = requests.post(url, json=config, headers={"x-api-key": API_KEY})
@@ -37,7 +39,6 @@ def get(fmt="Letter"):
 
 
 if __name__ == "__main__":
-    fmts = ['Letter', 'A4']
+    fmts = ["Letter", "A4"]
     for fmt in fmts:
         get(fmt=fmt).rename(f"static_pdf/resume.{fmt.lower()}.pdf")
-
